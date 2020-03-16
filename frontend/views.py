170426bceb33a7e1637d8f15ce1b746f0a543560
lizard-kim <rlaxodud1122@gmail.com>
2020-03-16 19:38:54 +0900

    post perfect

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from apply.models import Apply
 
 def main(request):
     return render(request, 'main.html')
 
 def apply(request):
-    return render(request, 'apply.html') 
+
+    if(request.method=='POST'):
+        
+        name = request.POST.get('name')
+        school_id = request.POST.get('schoolId')
+        phone_number = request.POST.get('phoneNumber')
+        email = request.POST.get('email')
+        major = request.POST.get('major')
+        tech_stack = request.POST.get('techStack')
+        motivation = request.POST.get('motivation')
+        idea = request.POST.get('idea')
+        
+        new_apply = Apply.objects.create(
+            name = name,
+            school_id = school_id,
+            phone_number = phone_number,
+            email = email,
+            major = major,
+            tech_stack = tech_stack,
+            motivation = motivation,
+            idea = idea
+        )
+        return redirect('/')
+    
+    else:
+        return render(request, 'apply.html') 
