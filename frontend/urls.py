abb7972b05f3da59cc9715a92f334cd766b61fa1
lizard-kim <rlaxodud1122@gmail.com>
2020-04-01 23:24:22 +0900

    make apply page unconnected

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,7 +4,7 @@ from .views import main, apply, login, admin, adminlist, detail
 
 urlpatterns = [
     path('', main, name="main"),
-    path('apply/', apply, name="apply"),
+    #path('apply/', apply, name="apply"),
     path('login/', login, name='login'),
     path('adminpage/', admin, name="admin"),
     path('adminlist/', adminlist, name="adminlist"),
